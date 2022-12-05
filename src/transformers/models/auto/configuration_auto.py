023f2b2555b7665065f7a4d1a713382bb005fbaa
kiansierra <kiansierra90@gmail.com>
2022-12-05 18:09:26 +0100

    configuration_auto in all caps as is model name

--- src/transformers/models/auto/configuration_auto.py
+++ src/transformers/models/auto/configuration_auto.py
@@ -361,7 +361,7 @@ MODEL_NAMES_MAPPING = OrderedDict(
         ("encoder-decoder", "Encoder decoder"),
         ("ernie", "ERNIE"),
         ("esm", "ESM"),
-        ("fan", "Fan"),
+        ("fan", "FAN"),
         ("flan-t5", "FLAN-T5"),
         ("flaubert", "FlauBERT"),
         ("flava", "FLAVA"),
